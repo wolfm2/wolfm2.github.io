c5a77e846d29baeced60c1254dc138561469a99d
wolfm2@newschool.edu <wolfm2@newschool.edu>
2017-11-28 19:48:15 -0500

    Alpha visualizations

--- 3Concepts/Narrative/local.js
+++ 3Concepts/Narrative/local.js
@@ -1,6 +1,24 @@
+/*
+TO VIS:
+1 Rising incar rates: line chart	
+    area chart / horizon chart by offense type
+2 incar by race / year: multi line, flipbook stacked bar chart
+3 Inmates moved to fed for overcrowding
+    ppl per state /  line %
+4 Felony disenfranchisement   1of13 b vs 1 of 56 nonb
+    People markers
+    Disenfranchisement rules by stateIncar & unsentenced (re: bail)
+5 buildout by state op capacity vs designed capacity
+    stacked area, multi line
+  
+Privatized prison rates 			
+suicide rates fed  : http://www.cnn.com/2017/04/19/health/prison-suicides-massachusetts-trnd/index.html
+
+*/
 
 $( document ).ready(function() {
 
+  // content
   $().timelinr({
       orientation: 'horizontal',
       // value: horizontal | vertical, default to horizontal
